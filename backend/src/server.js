b2b469e9e698637ba308677b6ac0feb91aa3422c
shivam20234158 <149950525+shivam20234158@users.noreply.github.com>
2025-06-20 00:49:38 +0530

    deployed the website

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser'; // Middleware to parse cookies
 import dotenv from 'dotenv'; 
 import cors from 'cors'; 
+import path from "path";
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.route.js';// Importing the auth routes 
 import chatRoutes from './routes/chat.route.js'; ;
@@ -10,6 +11,8 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT=process.env.PORT || 3006;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -25,6 +28,14 @@ app.use(cookieParser()); // Middleware to parse cookies
 app.use("/api/auth",authRoutes)
 app.use("/api/users",userRoutes)
 app.use("/api/chat",chatRoutes)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:3006/api" : "/api";
+
 export const axiosInstance=axios.create({
-    baseURL:"http://localhost:3006/api",
+    baseURL: BASE_URL,
     withCredentials:true,
 });
